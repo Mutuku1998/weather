dd0a15769aed216f4c821d518a35cf2a2a8103a9
mutuku1998 <martinmutuku1998@gmail.com>
2023-08-11 16:04:50 +0300

    api configuration
    adding two apis open weather api and geodbcities

---
+++ weatherapp/src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '498fefe5b7msh25974b3cccad9b9p19aa9bjsnd7596892c60e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+  };
+  
+  export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+  export const WEATHER_API_URL= 'https://api.openweathermap.org/data/2.5';
+  export const WEATHER_API_KEY ='5dc0cf4f5aaa0d1a67ee9c925ce95e03'
